505a2d0c51c944cd7bb81fcdc6e97c7d9f7666f3
Michael Tomae <michael.tomae@gmail.com>
2018-08-28 22:25:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ window.addEventListener("load", () => {
   //start your code here
   let outputResult = document.querySelector("output")
   let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('')
+  let upperCaseCharcters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+  
   
   document.addEventListener('submit', (event) => {
     event.preventDefault()
