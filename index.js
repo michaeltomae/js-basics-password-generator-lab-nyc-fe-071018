645a8b8e63e12fbeb31bee1b2a88c3b34c3e51fc
Michael Tomae <michael.tomae@gmail.com>
2018-08-28 22:34:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ window.addEventListener("load", () => {
     passwordLength = 4
   }
   let newPassword = ""
-  for (let i = 0; i < passwordLength; i++;){
+  for (let i = 0; i < passwordLength; i++;) {
   }   
   })
 });
