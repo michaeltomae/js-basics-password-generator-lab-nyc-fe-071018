2b84848ebccb4ed6caa437cb2a57127743096cf4
Michael Tomae <michael.tomae@gmail.com>
2018-08-28 22:49:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,24 @@ window.addEventListener("load", () => {
   if (passwordLength <4) {
     passwordLength = 4
   }
-  let newPassword = ""
+  let newPassword += ""
   for (let i = 0; i < passwordLength; i++) {
-    lowerCaseCharacters[math.floor(Math.random() * lowerCaseCharacters.length)]
+    
+    lowerCaseCharacters[
+      math.floor(Math.random() * lowerCaseCharacters.length)
+      ]
+  
+    newPassword += upperCaseCharacters[
+      math.floor(Math.random() * upperCaseCharacters.length)
+      ]
+
+    newPassword += numberCharacters[
+      math.floor(Math.random() * numberCharacters.length)
+      ]
+
+    newPassword += specialCharacters[
+      math.floor(Math.random() * specialCaseCharacters.length)
+      ]
   }
   console.log(newPassword)
   })
