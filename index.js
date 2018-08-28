cbdf77de35c2154ad4249efa8351fae207c9d63c
Michael Tomae <michael.tomae@gmail.com>
2018-08-28 22:21:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", () => {
   //start your code here
   let outputResult = document.querySelector("output")
+  let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split()
   
   document.addEventListener('submit', (event) => {
     event.preventDefault()
     let input = document.getElementById('passwordLength')
     let passwordLength = input.value
     console.log(passwordLength)
-    "abcdefghijklmnopqrstuvwxyz".split()
+
   })
 });
